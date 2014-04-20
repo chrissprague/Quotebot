cd8cce14250b125d6c91497bac50541515163c9a
Chris <secretevilradio@gmail.com>
2014-04-19 23:00:40 -0400

    fixed some issues with the bot, added script to run need to add to readme that u need a file in bin or wherever the python file is located named "quotes"

--- quotebot.py
+++ quotebot.py
@@ -1,7 +1,9 @@
 import random
+import sys
 def add_quote(string):
-	f = open("test", "r+")
+	f = open("quotes", "r+")
 	first_quote = True
+	original = string
 	while ( True ) :
 		if f.readline() == "":
 			break
@@ -11,17 +13,31 @@ def add_quote(string):
 	else:
 		string="\n"+(str)(string)
 	f.write(string)
-	f.close
+	print('Quote added: "'+original+'"')
+	f.close()
 def get_quote():
-	f = open("~/bin/.quotes")
+	f = open("quotes","r+")
 	num_lines = 0
 	for line in f:
 		num_lines+=1
 	count = 0
-	target = random.randInt(0, num_lines)
+	target = random.randint(0, num_lines)
+	f.close()
+	f=open("quotes","r+")
 	for line in f:
 		if count == target:
-			print(line)
-			return;
+			if line == '' or line == '\n' or line == '\0':
+				get_quote() # try again
+			elif '\n' not in line:
+				get_quote() # try again
+			else:
+				print(line,end='')
+				return;
 		count+=1
-add_quote("HeWorld!")
+	get_quote()
+if len(sys.argv) > 2:
+	exit()
+if len(sys.argv) == 2:
+	add_quote((str)(sys.argv[1]))
+else:
+	get_quote()
