e4777ae37832bdb53b8dd4623896108285c8a5e9
Chris <secretevilradio@gmail.com>
2014-05-14 10:19:47 -0400

    bug fixes, now supports --reset flag to reset quotes file

--- quotebot.py
+++ quotebot.py
@@ -1,18 +1,31 @@
 import random, sys, os
+
 absolute_path = os.path.expanduser('~/bin/quotes')
+
 def add_quote(string):
+
 	with open (absolute_path, 'a') as f: f.write (string+"\n")
 	print('Quote added: "'+string+'"')
 	f.close()
+
 def get_quote():
+
 	f = open(absolute_path,"r+")
+
+	if not os.stat(absolute_path).st_size > 0 : # file is empty - cannot fetch any quotes
+		print("Error: no quotes to grab!")
+		return;
+
 	num_lines = 0
 	for line in f:
 		num_lines+=1
+
 	count = 0
 	target = random.randint(0, num_lines+1)
 	f.close()
+
 	f=open(absolute_path,"r+")
+
 	for line in f:
 		if count == target:
 			if line == '' or line == '\n' or line == '\0':
@@ -25,13 +38,20 @@ def get_quote():
 				print(line,end='')
 				return;
 		count+=1
+
 	f.close()
-	get_quote()
+	get_quote() # try again
+
 if __name__ == "__main__":
 	if len(sys.argv) > 2:
 		print("Usage: quote [quote_to_add]")
 		exit()
+
 	if len(sys.argv) == 2:
 		add_quote((str)(sys.argv[1]))
+
 	else:
 		get_quote()
+
+
+
